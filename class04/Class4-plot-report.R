e5d7e8ca49116df9b2ec9618da9ab4ba633e05b4
IMAN SYED <imsyed@ucsd.edu>
2025-11-18 12:15:57 -0800

    Commit 4

---
+++ class04/Class4-plot-report.R
@@ -0,0 +1,13 @@
+
+
+#plot from week 4
+
+x <- 1:50
+
+plot(x,sin(x))
+
+#making it girlier ehehe!
+
+plot(x,sin(x),typ="l", col="purple", lwd=1.5, xlab= "My Anxiety levels")
+
+
---
+++ class04/class04.R
@@ -0,0 +1,2 @@
+x <- 1:50
+plot(x, sin(x), typ="l")
